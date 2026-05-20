55f93a22e507774b7a22526f7bef423de9f7debf
Arjan <ahoekdev@gmail.com>
2026-05-20 08:32:09 +0200

    update sanity config

--- apps/cms/sanity.config.ts
+++ apps/cms/sanity.config.ts
@@ -3,27 +3,11 @@ import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
-export default defineConfig([
-  {
-    basePath: '/production',
-    name: 'production',
-    title: 'Production',
-    projectId: 'c2n8vovr',
-    dataset: 'production',
-    plugins: [structureTool(), visionTool()],
-    schema: {
-      types: schemaTypes,
-    },
+export default defineConfig({
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
+  dataset: process.env.SANITY_STUDIO_DATASET || 'development',
+  plugins: [structureTool(), visionTool()],
+  schema: {
+    types: schemaTypes,
   },
-  {
-    basePath: '/development',
-    name: 'development',
-    title: 'Development',
-    projectId: 'c2n8vovr',
-    dataset: 'development',
-    plugins: [structureTool(), visionTool()],
-    schema: {
-      types: schemaTypes,
-    },
-  },
-])
+})
