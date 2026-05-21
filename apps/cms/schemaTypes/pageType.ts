d25ebafa61a2044eaf244df79607d570b1488e2b
Arjan Hoek <ahoekdev@gmail.com>
2026-05-21 07:35:47 +0200

    rename rich text to block content and fix bug

--- apps/cms/schemaTypes/pageType.ts
+++ apps/cms/schemaTypes/pageType.ts
@@ -1,17 +1,17 @@
 // ./schemas/pageType.ts
 
-import {defineArrayMember, defineField, defineType} from 'sanity'
+import { defineArrayMember, defineField, defineType } from 'sanity'
 
 export const pageType = defineType({
   name: 'page',
   type: 'document',
   title: 'Page',
   fields: [
-    defineField({name: 'title', type: 'string'}),
+    defineField({ name: 'title', type: 'string' }),
     defineField({
       name: 'slug',
       type: 'slug',
-      options: {source: 'title'},
+      options: { source: 'title' },
       validation: (Rule) => Rule.required(),
     }),
     defineField({
@@ -23,10 +23,10 @@ export const pageType = defineType({
           name: 'textWithHeading',
           type: 'textWithHeading',
         }),
-        // defineArrayMember({
-        //   name: 'richText',
-        //   type: 'richText',
-        // }),
+        defineArrayMember({
+          name: 'blockContent',
+          type: 'blockContent',
+        }),
       ],
     }),
   ],
