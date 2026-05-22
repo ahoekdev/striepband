a867cb5cb8b14ef1de72274e7b756127b887e7ca
Arjan Hoek <ahoekdev@gmail.com>
2026-05-22 07:11:21 +0200

    instal astro react plugin and react video player

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -6,19 +6,19 @@ import sanity from "@sanity/astro";
 
 import netlify from "@astrojs/netlify";
 
+import react from "@astrojs/react";
+
 const { SANITY_STUDIO_DATASET, SANITY_STUDIO_PROJECT_ID } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [
-    sanity({
-      projectId: SANITY_STUDIO_PROJECT_ID,
-      dataset: SANITY_STUDIO_DATASET,
-      useCdn: false, // for static builds
-    }),
-  ],
+  integrations: [sanity({
+    projectId: SANITY_STUDIO_PROJECT_ID,
+    dataset: SANITY_STUDIO_DATASET,
+    useCdn: false, // for static builds
+  }), react()],
   fonts: [
     {
       provider: fontProviders.google(),
